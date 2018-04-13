f3ebed379c4c7dbe17df3717b4f15c888eef434b
Edd Robinson <me@edd.io>
2018-04-13 19:31:22 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # qprof
-A tool for profiling the performance of InfluxQL queries
+
+A tool for profiling the performance of InfluxQL queries.
+
+`qprof` works by executing a provided query one or more times against an InfluxDB 
+server and profiling the server both before and after execution. Profiles are
+bundled into an archive for easy dissemination.
+
+Because `qprof` gathers profiles before and after all queries have executed, 
+delta profiles become possible, which can help to identify the cost directly 
+associated with running the queries.
+
+
---
+++ main.go
@@ -0,0 +1,302 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	client "github.com/influxdata/influxdb/client/v2"
+)
+
+type profile struct {
+	Name  string
+	Fname string
+	Debug int
+}
+
+// cpu must always be the first profile.
+var profiles = []profile{
+	{Name: "profile", Fname: "cpu.pb.gz"},
+	{Name: "block", Fname: "block.txt", Debug: 1},
+	{Name: "goroutine", Fname: "goroutine.txt", Debug: 1},
+	{Name: "heap", Fname: "heap.pb.gz", Debug: 1},
+	{Name: "mutex", Fname: "mutex.txt", Debug: 1},
+}
+
+const example = `
+Example usage: $ qprof -db mydb -d 5m "SELECT * FROM cpu WHERE tag1 = 'foo'"
+	
+`
+
+var (
+	host       string
+	user, pass string
+)
+
+var (
+	query string
+	db    string
+	n     int
+	d     time.Duration
+)
+
+var (
+	out string
+	cpu bool
+)
+
+var clt client.Client
+var err error
+
+// Used to store relevant data around the execution of this program.
+var infoBuf bytes.Buffer
+var archivePath string
+var totalExecutions int
+
+func main() {
+	flag.StringVar(&host, "host", "http://localhost:8086", "scheme://host:port of server/cluster/load balancer. (default: http://localhost:8086)")
+	flag.StringVar(&user, "user", "", "Username if using authentication (optional)")
+	flag.StringVar(&pass, "pass", "", "Password if using authentication (optional)")
+
+	flag.StringVar(&db, "db", "", "Database to query (required)")
+	flag.IntVar(&n, "n", 1, "Repeat query n times (default 1 if -d not specified)")
+	flag.DurationVar(&d, "t", 0, "Repeat query for this period of time (optional and overrides -n)")
+
+	flag.StringVar(&out, "out", ".", "Output directory (default pwd)")
+	flag.BoolVar(&cpu, "cpu", true, "Include CPU profile (will take at least 30s)")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Fprintln(os.Stderr, "Please provide query as positional argument:\n", example)
+		os.Exit(1)
+	} else if len(flag.Args()) > 1 {
+		fmt.Fprintln(os.Stderr, "Query partially parsed. Is it quoted properly?\n", example)
+		os.Exit(1)
+	}
+	query = flag.Arg(0)
+
+	// Store options set.
+	infoBuf.WriteString("Flags:\n")
+	flag.VisitAll(func(f *flag.Flag) {
+		if _, err := infoBuf.WriteString(fmt.Sprintf("-%s %v\n", f.Name, f.Value.String())); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	})
+
+	if !cpu {
+		profiles = profiles[1:]
+	}
+
+	// Check out directory is available.
+	if err := os.MkdirAll(out, 0600); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	archivePath = filepath.Join(out, "profiles.tar.gz")
+
+	// Check we can connect to the server.
+	if clt, err = NewClient(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer clt.Close()
+
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	log.Printf("All profiles gathered and saved at %s. Total query executions: %d.", archivePath, totalExecutions)
+}
+
+func run() error {
+	var allBuf bytes.Buffer // Buffer for entire archive.
+	var buf bytes.Buffer    // Temporary buffer for each profile result.
+
+	gz := gzip.NewWriter(&allBuf)
+	defer gz.Close()
+	tw := tar.NewWriter(gz)
+	defer tw.Close()
+
+	captureProfile := func(p profile) error {
+		if p.Name == "profile" {
+			log.Print("Capturing CPU profile. This will take 30s...")
+		}
+
+		if err := takeProfile(&buf, p.Name, p.Debug); err != nil {
+			return err
+		}
+
+		err := tw.WriteHeader(&tar.Header{
+			Name:    p.Fname,
+			Mode:    0600,
+			ModTime: time.Now().UTC(),
+			Size:    int64(buf.Len()),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Write the profile file's data to the tar writer.
+		if _, err = io.Copy(tw, &buf); err != nil {
+			return err
+		}
+
+		// Reset the buffer for the next profile.
+		buf.Reset()
+		log.Printf("%q profile captured...\n", p.Name)
+		return nil
+	}
+
+	// Take the base profiles.
+	for _, p := range profiles {
+		p.Fname = fmt.Sprintf("base-%s", p.Fname)
+		if err := captureProfile(p); err != nil {
+			return err
+		}
+	}
+
+	// Run the queries
+	log.Print("Begin query execution...")
+	if d == 0 {
+		for i := 0; i < n; i++ {
+			if err := runQuery(); err != nil {
+				return err
+			}
+		}
+	} else {
+		timer := time.NewTimer(d)
+	OUTER:
+		for {
+			select {
+			case <-timer.C:
+				log.Printf("Queries executed for at least %v", d)
+				break OUTER
+			default:
+				if err := runQuery(); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Take the final profiles
+	log.Print("Taking final profiles...")
+	for _, p := range profiles {
+		if err := captureProfile(p); err != nil {
+			return err
+		}
+	}
+
+	// Finally, write the general data about the running of this program.
+	infoBuf.WriteString(fmt.Sprintf("\n Total executions: %d", totalExecutions))
+	err := tw.WriteHeader(&tar.Header{
+		Name:    "info.txt",
+		Mode:    0600,
+		ModTime: time.Now().UTC(),
+		Size:    int64(infoBuf.Len()),
+	})
+	if err != nil {
+		return err
+	}
+
+	// Write the info data to the tar writer.
+	if _, err = io.Copy(tw, &infoBuf); err != nil {
+		return err
+	}
+
+	// Close the tar writer.
+	if err := tw.Close(); err != nil {
+		return err
+	}
+
+	// Close the gzip writer.
+	if err := gz.Close(); err != nil {
+		return err
+	}
+
+	fd, err := os.Create(filepath.Join(out, "profiles.tar.gz"))
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	_, err = io.Copy(fd, &allBuf)
+	return err
+}
+
+// takeProfile takes the named profile and writes the result to w.
+func takeProfile(w io.Writer, name string, debug int) error {
+	u, err := url.Parse(host)
+	if err != nil {
+		return err
+	}
+	u.Path = path.Join("/debug/pprof/", name)
+
+	if debug > 0 {
+		q := url.Values{"debug": []string{fmt.Sprint(debug)}}
+		u.RawQuery = q.Encode()
+	}
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(w, resp.Body)
+	return err
+}
+
+// NewClient returns a new InfluxDB client, which has successfully connected to
+// a running server.
+func NewClient() (client.Client, error) {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     host,
+		Username: user,
+		Password: pass,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	dur, _, err := c.Ping(2 * time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Host %s responded to a ping in %v\n", host, dur)
+	return c, nil
+}
+
+// runQuery executes query against the cluster.
+func runQuery() error {
+	totalExecutions++
+
+	now := time.Now()
+	defer func() {
+		took := time.Since(now)
+		msg := fmt.Sprintf("Query %q took %v to execute.", query, took)
+
+		log.Print(msg)
+
+		// Update info.txt
+		infoBuf.WriteString(msg + "\n")
+	}()
+
+	resp, err := clt.Query(client.NewQuery(query, db, ""))
+	if err != nil {
+		return err
+	}
+	return resp.Error()
+}
